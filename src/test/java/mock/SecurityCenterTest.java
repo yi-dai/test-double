34eb61749a943cd9b2ed26f4dbde9e049caa97e7
yi-dai <851758415@qq.com>
2020-08-04 13:00:43 +0800

    add code to realize mock based on mockito.

--- src/test/java/mock/SecurityCenterTest.java
+++ src/test/java/mock/SecurityCenterTest.java
@@ -3,7 +3,15 @@ package mock;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.mockito.Mockito.verify;
+
+
+@ExtendWith(MockitoExtension.class)
 public class SecurityCenterTest {
     /* 需求描述：
     编写SecurityCenter类的单元测试，单元测试switchOn方法，不依赖于DoorPanel的close的方法实现
@@ -13,7 +21,14 @@ public class SecurityCenterTest {
     public void setUp() {
     }
 
+    @Mock
+    private DoorPanel mockDoorPanel;
+    @InjectMocks
+    private SecurityCenter securityCenter;
+
     @Test
     public void shouldVerifyDoorIsClosed() {
+        securityCenter.switchOn();
+        verify(mockDoorPanel).close();
     }
 }
